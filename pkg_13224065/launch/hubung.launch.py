6e85669baebef518aaa74c34c17629a3d9f56f29
Jouvan <jouvan294@gmail.com>
2025-08-06 00:43:04 +0700

    Added Comments To .py

--- pkg_13224065/launch/hubung.launch.py
+++ pkg_13224065/launch/hubung.launch.py
@@ -1,14 +1,19 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+# File Launch Digunakan untuk menjalankan node hubung_node
+# Node ini menghubungkan data dari topik autonomous_vel ke cmd_vel dan cmd_type
+# Dengan parameter yang diambil dari file config/config.yaml
 def generate_launch_description():
-    return LaunchDescription([
+    #Merupakan fungsi yang mengembalikan LaunchDescription
+    return LaunchDescription([    
+        # Node hubung_node yang akan dijalankan
         Node(
-            package='pkg_13224065',
-            executable='node_hubung',
-            name='hubung_node',
-            parameters=['config/hubung_params.yaml'],
-            output='screen'
+            package='pkg_13224065',                 # Nama package yang berisi node
+            executable='node_hubung',               # Nama executable dari node yang akan dijalankan
+            name='hubung_node',                     # Nama node yang akan digunakan
+            parameters=['config/config.yaml'],      # Menggunakan file config config.yaml untuk parameter
+            output='screen'                         # Output dari node akan ditampilkan di terminal
         )
     ])
 
--- pkg_13224065/pkg_13224065/node_hubung.py
+++ pkg_13224065/pkg_13224065/node_hubung.py
@@ -1,16 +1,28 @@
+#Import library ROS2 yang diperlukan
 import rclpy
 from rclpy.node import Node
 
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 
+#Definisikan kelas HubungNode yang merupakan turunan dari Node
+#Node ini akan menghubungkan data dari topik autonomous_vel ke cmd_vel dan cmd_type
+#Dengan parameter yang diambil dari file config/config.yaml
 class HubungNode(Node):
+    #Difinesikan konstruktor untuk inisialisasi node
     def __init__(self):
+        #Inisialisasi node dengan nama 'hubung_node'
         super().__init__('hubung_node')
 
+        #Membuat publisher untuk topik cmd_vel dan cmd_type
+        #cmd_vel untuk mengirimkan data ke robot
+        #cmd_type untuk mengirimkan tipe perintah yang digunakan
+        #Publisher ini akan mengirimkan pesan dengan tipe Twist untuk cmd_vel dan String untuk cmd
         self.cmd_vel_publisher = self.create_publisher(Twist, 'cmd_vel', 10)
         self.cmd_type_publisher = self.create_publisher(String, 'cmd_type', 10)
 
+        #Membuat subscriber untuk topik autonomous_vel
+        #Subscriber ini akan menerima pesan dengan tipe Twist dari topik autonomous_vel
         self.autonomous_vel_subscriber = self.create_subscription(
             Twist,
             'autonomous_vel',
@@ -18,6 +30,8 @@ class HubungNode(Node):
             10
         )
 
+    # Callback function yang akan dipanggil ketika menerima pesan dari topik autonomous_vel
+    #Fungsi ini akan meneruskan pesan ke cmd_vel dan mengirimkan tipe perintah 'autonomous' ke cmd_type
     def listener_callback(self, msg):
         self.cmd_vel_publisher.publish(msg)
 
@@ -25,6 +39,10 @@ class HubungNode(Node):
         cmd_type_msg.data = 'autonomous'
         self.cmd_type_publisher.publish(cmd_type_msg)
 
+#Merupakan fungsi utama untuk menjalankan node
+#Fungsi ini akan menginisialisasi rclpy, membuat instance dari HubungNode,
+#dan menjalankan node tersebut hingga dihentikan
+#Setelah selesai, node akan dihancurkan dan rclpy akan dimatikan
 def main(args=None):
     rclpy.init(args=args)
 
